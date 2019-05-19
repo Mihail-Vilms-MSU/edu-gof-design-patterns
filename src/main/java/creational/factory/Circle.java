5bd07307fec16b599cdf8c7a388cf5c75b7de85c
Mikhail Vilms <mihail.vilms@gmail.com>
2019-05-19 15:09:44 +0300

    FEAT: Factory pattern implementation + tests

---
+++ src/main/java/creational/factory/Circle.java
@@ -0,0 +1,20 @@
+package creational.factory;
+
+public class Circle extends Shape{
+
+    private float radius;
+
+    @Override
+    public float getPerimeter() {
+        return (float)3.14*2*radius;
+    }
+
+    @Override
+    public float getArea() {
+        return (float)3.14*radius*radius;
+    }
+
+    public Circle(float radius){
+        this.radius = radius;
+    }
+}
