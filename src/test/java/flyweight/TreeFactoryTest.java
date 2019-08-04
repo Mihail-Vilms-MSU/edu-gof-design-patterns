acdb9c0434f7b128c4d6b8b743ff5b4d01ed40ea
Mikhail <mihail.vilms@gmail.com>
2019-08-04 00:12:26 -0700

    FEAT: "Flyweight" pattern example has been added

---
+++ src/test/java/flyweight/TreeFactoryTest.java
@@ -0,0 +1,24 @@
+package flyweight;
+
+import org.junit.Test;
+import structural.flyweight.Tree;
+import structural.flyweight.TreeFactory;
+import structural.flyweight.TreeModel;
+
+public class TreeFactoryTest {
+
+    @Test
+    public void creatingTrees(){
+        TreeModel oakModel = TreeFactory.getTreeType("Oak", "Red", "Oak's additional data");
+        Tree oak = new Tree(10, 20, oakModel);
+        oak.draw();
+
+        TreeModel mapleModel = TreeFactory.getTreeType("Maple", "Yellow", "Maple's additional data");
+        Tree maple = new Tree(20, 30, mapleModel);
+        maple.draw();
+
+        TreeModel sameOakModel = TreeFactory.getTreeType("Oak", "***", "***");
+        Tree anotherOak = new Tree(40, 50, oakModel);
+        anotherOak.draw();
+    }
+}
